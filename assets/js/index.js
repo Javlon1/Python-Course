d7cbc26f2ec99376e8455291742c79cf6539d754
Javlon1 <muhamedmijotot@gmail.com>
2024-01-01 18:18:57 +0500

    Python-Course

---
+++ assets/js/index.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const scrollUpButton = document.getElementById('scrollUp');
+
+    const handleScroll = () => {
+        const scrollY = window.scrollY;
+        const threshold = 100;
+        if (scrollY > threshold) {
+            scrollUpButton.classList.add('visible');
+        } else {
+            scrollUpButton.classList.remove('visible');
+        }
+    };
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        });
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    scrollUpButton.addEventListener('click', scrollToTop);
+});
