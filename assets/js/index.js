420940e226aad4579fe3c31c3f9b769168821a2a
Javlon1 <muhamedmijotot@gmail.com>
2024-01-06 13:55:09 +0500

    Сarousel start

--- assets/js/index.js
+++ assets/js/index.js
@@ -59,8 +59,68 @@ function mentorsModal(id) {
 
 
 // slider start
+//
+document.addEventListener("DOMContentLoaded", function () {
+    const carouselContainer = document.getElementById('carouselContainer');
+    const items = document.querySelectorAll('.carousel__list__item');
+    const totalItems = items.length;
 
+    let currentIndex = 1;
+    let isMouseOverCarousel = false;
 
+    function initializeCarousel() {
+        for (let i = 0; i < totalItems; i++) {
+            const cloneItem = items[i].cloneNode(true);
+            carouselContainer.appendChild(cloneItem);
+        }
+
+        showSlide(currentIndex);
+
+        setInterval(() => {
+            if (!isMouseOverCarousel) {
+                nextSlide();
+            }
+        }, 3000);
+    }
+
+    function showSlide(index) {
+        const offset = -index * 288;
+        carouselContainer.style.transform = `translateX(${offset}px)`;
+    }
+
+    function prevSlide() {
+        if (currentIndex > 0) {
+            currentIndex--;
+        } else {
+            currentIndex = totalItems - 1;
+        }
+
+        showSlide(currentIndex);
+    }
+
+    function nextSlide() {
+        if (currentIndex < totalItems * 2 - 1) {
+            currentIndex++;
+        } else {
+            currentIndex = totalItems;
+        }
+
+        showSlide(currentIndex);
+    }
+
+    initializeCarousel();
+
+    carouselContainer.addEventListener('mouseenter', () => {
+        isMouseOverCarousel = true;
+    });
+
+    carouselContainer.addEventListener('mouseleave', () => {
+        isMouseOverCarousel = false;
+    });
+});
+// 
+
+//
 function playPause(videoId) {
     var video = document.getElementById(videoId);
     if (video) {
@@ -73,6 +133,7 @@ function playPause(videoId) {
         console.error("Элемент с id '" + videoId + "' не найден");
     }
 }
+// 
 // slider end
 
 
